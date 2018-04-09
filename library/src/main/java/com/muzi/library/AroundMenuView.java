badf31f6efe1e6b4b551fd2b94b60f3634ece970
muzi <727784430@qq.com>
2018-04-09 23:54:14 +0800

    开始啦

--- library/src/main/java/com/muzi/library/AroundMenuView.java
+++ library/src/main/java/com/muzi/library/AroundMenuView.java
@@ -1,6 +1,9 @@
 package com.muzi.library;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -12,36 +15,50 @@ import android.view.View;
 public class AroundMenuView extends View {
 
     private int widthMeasure, heightMeasure;
-    private int minSize = 60;
+
+    private int radius;//半径
+
+    private int minSize = 130;//最小尺寸
+
+    private Paint paint;
 
     public AroundMenuView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public AroundMenuView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.FILL);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int widthMeasure, heightMeasure;
         int widthMode, heightMode;
         widthMeasure = MeasureSpec.getSize(widthMeasureSpec);
         widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        if (widthMode == MeasureSpec.EXACTLY) {
-            if (widthMode == MeasureSpec.AT_MOST) {
-                widthMeasure = minSize;
-            }
+
+        if (widthMode == MeasureSpec.AT_MOST) {
+            widthMeasure = minSize;
         }
 
-        heightMeasure = MeasureSpec.getSize(widthMeasureSpec);
-        heightMode = MeasureSpec.getMode(widthMeasureSpec);
-        if (heightMode == MeasureSpec.EXACTLY) {
-            if (heightMode == MeasureSpec.AT_MOST) {
-                heightMeasure = minSize;
-            }
+        heightMeasure = MeasureSpec.getSize(heightMeasureSpec);
+        heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        if (heightMode == MeasureSpec.AT_MOST) {
+            heightMeasure = minSize;
         }
-        setMeasuredDimension(widthMeasure,heightMeasure);
+        radius = widthMeasure / 2;
+        setMeasuredDimension(widthMeasure, heightMeasure);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        canvas.drawCircle(radius, radius, radius, paint);
     }
 }
